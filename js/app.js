6a2ec878e630ef28a5e5574cd51f32dce1891f3e
R. Terrado <r.terrado@gmail.com>
2016-07-18 09:20:06 -0700

    Added sound

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	$('.ryu').mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	$('.ryu').mousedown(function(){
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left','520px');
+			});
+	})
+		$('.ryu').mouseup(function(){
+		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
+	})
+});
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
